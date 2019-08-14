74d5332348c4d5944a53e3576bc2e43b115a6419
natalialexandra <natalia.alexandra195@gmail.com>
2019-08-14 15:47:05 +0200

    add exerciece string-number

---
+++ strings_numbers/index.js
@@ -0,0 +1,59 @@
+// console.log(`${}`);
+console.log("---------------------------");
+// 1
+const name = "Natalia";
+console.log(`Name length is ${name.length}`);
+const secondElem = name[1];
+const sixthElem = name[5];
+console.log(`2nd element: ${secondElem}, 6th element: ${sixthElem}`);
+
+
+console.log("---------------------------");
+// 2
+const js = "JavaScript";
+console.log(`length of string is ${js.length} characters`);
+const letterJ = js[0];
+const letterS = js[4];
+console.log(`${letterJ} & ${letterS}`);
+
+console.log("---------------------------");
+// 3
+const sentence = "I can walk in the park all day!";
+const goal = sentence.includes("park");
+const park = (goal)? "park" : "keep trying";
+console.log(`${park}`);
+//console.log(`${sentence.substring(18, 22)}`);
+
+
+console.log("---------------------------");
+// 4
+const jsWord = "JavaScript";
+console.log(`${jsWord.substring(3, 6)}`);
+
+
+console.log("---------------------------");
+// 5
+const helloWorld = "Hello World";
+console.log(`${helloWorld.toUpperCase()}`);
+
+
+console.log("---------------------------");
+// 6
+const anotherWorld = "Hello Earthling";
+console.log(`${anotherWorld.toLowerCase()}`);
+
+
+console.log("---------------------------");
+// 7
+const shoes = "nice shoes";
+console.log(`Does the string include a "l"? ${shoes.includes("l")}
+Does the string include a "n"? ${shoes.includes("n")}`);
+
+
+console.log("---------------------------");
+// 8
+const firstString = "JavaScript";
+console.log(`${firstString[0]}${firstString}${firstString[0]}`);
+
+console.log("---------------------------");
+// 9
--- ternary/index.js
+++ ternary/index.js
@@ -1,29 +1,38 @@
-// ( >= ) ? "" : "";
+// ( ) ? "true" : "false";
+console.log("-------------------------------------");
 // 1
 const legalAge = 18;
 const agePerson1 = 25;
 const canDrive = (agePerson1 >= legalAge) ? "Yes, you can drive! Hope you have a license" : "Nope, you're too young!";
 console.log(`${canDrive}`);
 
+
 console.log("-------------------------------------");
 // 2
 const speedLimit = 130;
 const speedCurrent = 90;
-const speedCheck = (speedCurrent >= speedLimit) ? "You're going too fast - slow down!" : "You're driving below the speed limit";
+const speedCheck = (speedCurrent > speedLimit) ? "You're going too fast - slow down!" : "You're driving below the speed limit";
 console.log(`${speedCheck}`);
+
 
 console.log("-------------------------------------");
 // 3
 const legalAge2 = 21;
 const personAge = 20;
-const checkAge = (personAge >= legalAge2) ? "Here's some juice" : "Here's some wine";
+const checkAge = (personAge < legalAge2) ? "Here's some juice" : "Here's some wine";
 console.log(`${checkAge}`);
+
 
 console.log("-------------------------------------");
 // 4
-const student = false;
+const student = true;
 const isStudent = (student) ? "Ticket costs €5,00" : "Ticket costs €12,00";
 console.log(`${isStudent}`);
+// another solution
+const anotherStudent = "yes";
+const isStudent2 = (anotherStudent === "yes") ? "Ticket costs €5,00" : "Ticket costs €12,00";
+console.log(`another student ${isStudent2}`);
+
 
 console.log("-------------------------------------");
 // 5
@@ -31,3 +40,7 @@ const morning = 12;
 const evening = 13;
 const timeOfDay = (morning < evening) ? "Good Morning" : "Good Evening";
 console.log(`${timeOfDay}`);
+// another solution
+const morning2 = false;
+const timeOfDay2 = (morning2) ? "Good Morning" : "Good Evening";
+console.log(`another solution: ${timeOfDay2}`);
