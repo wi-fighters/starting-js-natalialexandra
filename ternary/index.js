9389d0e6b77782779aae2d6ca370d6e9a0563f43
natalialexandra <natalia.alexandra195@gmail.com>
2019-08-14 10:28:37 +0200

    done ternary exerciece

---
+++ ternary/index.js
@@ -0,0 +1,33 @@
+// ( >= ) ? "" : "";
+// 1
+const legalAge = 18;
+const agePerson1 = 25;
+const canDrive = (agePerson1 >= legalAge) ? "Yes, you can drive! Hope you have a license" : "Nope, you're too young!";
+console.log(`${canDrive}`);
+
+console.log("-------------------------------------");
+// 2
+const speedLimit = 130;
+const speedCurrent = 90;
+const speedCheck = (speedCurrent >= speedLimit) ? "You're going too fast - slow down!" : "You're driving below the speed limit";
+console.log(`${speedCheck}`);
+
+console.log("-------------------------------------");
+// 3
+const legalAge2 = 21;
+const personAge = 20;
+const checkAge = (personAge >= legalAge2) ? "Here's some juice" : "Here's some wine";
+console.log(`${checkAge}`);
+
+console.log("-------------------------------------");
+// 4
+const student = false;
+const isStudent = (student) ? "Ticket costs €5,00" : "Ticket costs €12,00";
+console.log(`${isStudent}`);
+
+console.log("-------------------------------------");
+// 5
+const morning = 12;
+const evening = 13;
+const timeOfDay = (morning < evening) ? "Good Morning" : "Good Evening";
+console.log(`${timeOfDay}`);
---
+++ ternary/ternary.md
@@ -0,0 +1,17 @@
+# Ternary Worksheet
+## Use the ternary operator to solve the tasks. Remember: Print your results to the console. 
+
+### 1. canDrive?
+ Declare a variable named canDrive. Check if someone's age is over 18. If so, print `Yes, you can drive! Hope you have a license` and if not, print `Nope, you're too young!`
+
+### 2. Slow Down! 
+Declare a variable named speedCheck. If speed is above 130km, print `You're going too fast - slow down!`. If speed is lower than 130km, print `You're driving below the speed limit`.
+
+### 3. How old are you?
+Declare a variable named personAge. If age is below 21, print `Here's some juice`. Otherwise print `Here's some wine`.
+
+### 4. Student Discount
+Declare a variable named isStudent. If true, print `Ticket costs €5,00`. If false, print `Ticket costs €12,00`.
+
+### 5. What time is it? 
+Declare a variable named timeOfDay. Check if the time of day is `morning` - if so, print `Good Morning`. If not, print `Good Evening`.
