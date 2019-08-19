d742bf1a0c9e3c7bc954a136edd203f5e27d3f89
natalialexandra <natalia.alexandra195@gmail.com>
2019-08-19 16:11:34 +0200

    add math

--- arithmetics/math.js
+++ arithmetics/math.js
@@ -1,3 +1,22 @@
 console.log("\n# 1 -----------------------------");
 //# 1 Lowest Number
+const lowestNum = Math.min(5, 3, 1, 100, 50);
+console.log(`lowest number: ${lowestNum}`);
 
+console.log("\n# 2 -----------------------------");
+//# 2 Highest Number
+const highestNum = Math.max(5, 10, 50, 100, 3);
+console.log(`highest number: ${highestNum}`);
+
+console.log("\n# 3 -----------------------------");
+//# 3 Rounding
+// round up
+let num1 = 3321.32321;
+let numRoundUp1 = Math.ceil(num1);
+console.log(`round up number 1: ${numRoundUp1}`);
+
+// round down
+
+
+console.log("\n# 4 -----------------------------");
+//# 4 random integer from 1-6
