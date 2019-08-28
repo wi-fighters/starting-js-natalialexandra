ae129b57778c9bcb1cbd01732b221dc1c80e480c
natalialexandra <natalia.alexandra195@gmail.com>
2019-08-28 10:44:07 +0200

    done exercise convert celsius to fahrenheit and fahrenheit to celsius

--- functions/temperature-converter.js
+++ functions/temperature-converter.js
@@ -1,6 +1,34 @@
-// The Temperature Converter
+/* The Temperature Converter
 
-function celsiusToFahrenheit(celsius){
-    
-    return `${}°F is ${}°F`;
+Create a function called celsiusToFahrenheit:
+
+    Store a celsius temperature into a variable.
+    Convert it to fahrenheit and output "NN°C is NN°F".
+
+Create a function called fahrenheitToCelsius:
+
+    Now store a fahrenheit temperature into a variable.
+    Convert it to celsius and output "NN°F is NN°C."
+*/
+
+/**
+ *  two functions to convert celsius to fahrenheit and fahrenheit to celsius
+ * @param {number} celsius
+ * @param {number} fahrenheit
+ * @return {string} message
+ * @example
+ *      celsiusToFahrenheit(20)
+ *  and fahrenheitToCelsius(20)
+ *
+ */
+function celsiusToFahrenheit(celsius) {
+  let convertCelsius = (celsius * 9) / 5 + 32;
+  return `${celsius}°C is ${convertCelsius}°F`;
 }
+console.log(celsiusToFahrenheit(20));
+
+function fahrenheitToCelsius(fahrenheit) {
+  let convertFahrenheit = ((fahrenheit - 32) / 9) * 5;
+  return `${fahrenheit}°F is ${convertFahrenheit}°C`;
+}
+console.log(fahrenheitToCelsius(20));
