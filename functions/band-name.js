fbb9e3371fb4e6003cf75104c7518c9c1b6d26a3
natalialexandra <natalia.alexandra195@gmail.com>
2019-08-28 20:48:08 +0200

    add global arry to store result for band-names

--- functions/band-name.js
+++ functions/band-name.js
@@ -1,3 +1,6 @@
+// global array to store band names
+let storeBandNames = [];
+
 function generateBandName(clothingColour, lastFoodEaten) {
   //capitalize first letter of color and of food
   let colorCapitalLetter1 =
@@ -8,4 +11,12 @@ function generateBandName(clothingColour, lastFoodEaten) {
   // return the message
   return `The ${colorCapitalLetter1} ${foodCapitalLetter1}`;
 }
-console.log(bandName("blue", "banana"));
+//console.log(generateBandName("blue", "banana"));
+
+//let bandName1 = generateBandName("blue", "banana");
+//storeBandNames.push(bandName1);
+storeBandNames.push(generateBandName("blue", "banana"));
+storeBandNames.push(generateBandName("green", "apple"));
+storeBandNames.push(generateBandName("black", "cookies"));
+
+console.log(storeBandNames);
